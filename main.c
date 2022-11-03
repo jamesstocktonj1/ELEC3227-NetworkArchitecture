03c061c41869eae0890ed47e8ec6123a2dce4e24
js3g20 <js3g20@soton.ac.uk>
2022-11-03 13:46:50 +0000

    Add File Imports

--- main.c
+++ main.c
@@ -4,6 +4,11 @@
 #include "include/io.h"
 #include "include/serial.h"
 
+#include "include/config.h"
+#include "include/application.h"
+#include "include/datalink.h"
+#include "include/network.h"
+
 
 int main() {
     init_pins();
