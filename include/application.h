64314ee355f4a63bb44b57b077b43cdc8cec86cd
js3g20 <js3g20@soton.ac.uk>
2022-11-19 17:17:54 +0000

    Add Encryption/Decryption

--- include/application.h
+++ include/application.h
@@ -4,4 +4,13 @@
 #include "config.h"
 
 
+// application functions
+void set_light(uint8_t lightID, uint8_t lightIntensity);
+
+
+// support functions
+void encrypt_data(uint8_t *data, uint8_t len, uint16_t key);
+void decrypt_data(uint8_t *data, uint8_t len, uint16_t key);
+
+
 #endif
--- src/application.c
+++ src/application.c
@@ -1,2 +1,18 @@
 #include "../include/application.h"
 
+
+void encrypt_data(uint8_t *data, uint8_t len, uint16_t key) {
+
+    for(int i=0; i<len; i++) {
+        if((i % 2) == 0) {
+            data[i] = data[i] ^ (0xff & (key >> 8));
+        }
+        else {
+            data[i] = data[i] ^ (0xff & key);
+        }
+    }
+}
+
+void decrypt_data(uint8_t *data, uint8_t len, uint16_t key) {
+    encrypt_data(data, len, key);
+}
