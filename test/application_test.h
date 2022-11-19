cba708395a4cbf3c82ee1b5eece648971bd34940
js3g20 <js3g20@soton.ac.uk>
2022-11-19 16:06:39 +0000

    Add Encryption/Decryption Tests

--- test/application_test.h
+++ test/application_test.h
@@ -6,9 +6,15 @@
 
 #include <assert.h>
 #include <stdio.h>
-#include <string.h>
 
 
 void main_application_test(void);
+
+uint8_t compare_data(uint8_t *data1, uint8_t *data2, uint8_t len);
+void print_data(uint8_t *data1, uint8_t *data2, uint8_t len);
+
+void encrypt_data_test(void);
+void decrypt_data_test(void);
+void dual_encryption_test(void);
 
 #endif
