042c97ab73367cacdbac953517a2e6f05ed86fa1
js3g20 <js3g20@soton.ac.uk>
2022-11-21 11:32:01 +0000

    Add Transport Tests

--- test/main.c
+++ test/main.c
@@ -2,6 +2,7 @@
 
 #include "../include/config.h"
 #include "application_test.h"
+#include "transport_test.h"
 #include "network_test.h"
 #include "datalink_test.h"
 #include "config_test.h"
@@ -12,6 +13,7 @@ int main() {
     main_config_test();
 
     main_application_test();
+    main_transport_test();
     main_network_test();
     main_datalink_test();
 }
