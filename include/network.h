c9d6d61cc2759121fc22b941b5f3a644db566e06
przemekff <p.forys2000@users.noreply.github.com>
2022-11-27 13:50:44 +0000

    Constants, Function headers

--- include/network.h
+++ include/network.h
@@ -3,5 +3,54 @@
 
 #include "util.h"
 
+#define NET_MAX_SEGMENT_SIZE 121
+#define NET_MAX_PACKET_SIZE 128
+
+#define ROUTE_TIMEOUT_MS 2000
+#define REQUEST_TIMEOUT_MS 500
+#define REQUEST_MAX_AMOUNT 3
+#define DEFAULT_NETWORK_SIZE 10
+
+#define RREQ_ID 0
+#define RREP_ID 1
+#define RERR_ID 2
+#define DATA_ID 3
+
+#define CONTROL_1_BYTE 0
+#define CONTROL_2_BYTE 1
+#define SRC_ADDRESS_BYTE 2
+#define DEST_ADDRESS_BYTE 3 
+#define LENGTH_BYTE 4
+#define TRAN_SEGMENT_BYTE 5
+
+#define RREQ_ORIG_ADDRESS_BYTE 5
+#define RREQ_ORIG_SEQ_BYTE 6
+#define RREQ_DEST_SEQ_BYTE 7
+#define RREQ_RREQ_ID_BYTE 8
+#define RREQ_HOP_COUNT_BYTE 9
+
+#define RREP_RREQ_DEST_ADDRESS 5 
+#define RREP_DEST_SEQ 6
+#define RREP_HOP_COUNT 7
+
+
+
+typedef struct {
+    uint8_t dest_node;
+    uint8_t next_hop; 
+    uint8_t dest_seq;
+    uint8_t hop_count;
+} net_rt_entry;
+
+
+void net_init(uint8_t node_address);
+
+bool net_handle_rx_packet(uint8_t *packet);
+
+bool net_handle_rreq(uint8_t *packet);
+void net_handle_rrep(uint8_t *packet);
+void net_handle_rerr(uint8_t *packet);
+void net_handle_data(uint8_t *packet);
+
 
 #endif
--- src/network.c
+++ src/network.c
@@ -1,1 +1,42 @@
 #include "../include/network.h"
+
+static uint8_t net_seqnum; 
+static net_rt_entry *route_table;
+static uint8_t RREQ_ID_buffer;
+static uint8_t net_node_address;
+
+
+
+void net_init(uint8_t node_address){
+    net_seqnum = 0;
+    route_table = calloc(DEFAULT_NETWORK_SIZE, sizeof(net_rt_entry));
+    RREQ_ID_buffer = 0;
+    net_node_address = node_address;
+}
+
+
+bool net_handle_rx_packet(uint8_t *packet){
+
+    uint8 masked = data[0] & (3<<6);
+    switch (masked)
+    {
+        case RREQ_ID: net_handle_rreq(*data);
+        case RREP_ID: net_handle_rrep(*data);
+        case RERR_ID: net_handle_rerr(*data);
+        case DATA_ID: net_handle_data(*data);
+    }
+};
+
+bool net_handle_rreq ( uint8_t *packet){
+    
+    net_rt_entry *rt_entry = route_table(packet[SRC_ADDRESS_BYTE]);
+    rt_entry->dest_node = packet[RREQ_ORIG_ADDRESS_BYTE];
+    rt_entry->next_hop = packet[SRC_ADDRESS_BYTE];
+    rt_entry->dest_seq = packet[RREQ_ORIG_SEQ_BYTE];
+    rt_entry->hop count = packet[RREQ_HOP_COUNT_BYTE] + 1;
+
+
+
+
+
+}
