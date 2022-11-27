894b672e36af47d9f50dd6e109c814cb8d761aaa
przemekff <p.forys2000@users.noreply.github.com>
2022-11-27 15:43:32 +0000

    implemented rreq handler

--- src/network.c
+++ src/network.c
@@ -20,7 +20,12 @@ bool net_handle_rx_packet(uint8_t *packet){
     uint8 masked = data[0] & (3<<6);
     switch (masked)
     {
-        case RREQ_ID: net_handle_rreq(*data);
+        case RREQ_ID: 
+            if(net_handle_rreq(*data));
+                send_rrep(*data);
+            else
+                rebroadcast_rreq(*data);
+
         case RREP_ID: net_handle_rrep(*data);
         case RERR_ID: net_handle_rerr(*data);
         case DATA_ID: net_handle_data(*data);
@@ -28,15 +33,32 @@ bool net_handle_rx_packet(uint8_t *packet){
 };
 
 bool net_handle_rreq ( uint8_t *packet){
+
+    net_rt_entry *rt_entry_src = route_table(packet[SRC_ADDRESS_BYTE]);
+    net_rt_entry *rt_entry_dest = route_table(packet[DEST_ADDRESS_BYTE]);
+
+    if((rt_entry_src->dest_seq < packet[RREQ_ORIG_SEQ_BYTE]) || ((rt_entry_src->dest_seq == packet[RREQ_ORIG_SEQ_BYTE]) && rt_entry_src->hop_count > packet[RREQ_HOP_COUNT_BYTE] ) ){
+
+    rt_entry_src->dest_node = packet[RREQ_ORIG_ADDRESS_BYTE];
+    rt_entry_src->next_hop = packet[SRC_ADDRESS_BYTE];
+    rt_entry_src->dest_seq = packet[RREQ_ORIG_SEQ_BYTE];
+    rt_entry_src->hop count = packet[RREQ_HOP_COUNT_BYTE] + 1;
+
+    }
+
+
+    if (packet[DEST_ADDRESS_BYTE] == net_node_address || rt_entry_dest->dest_seq > packet[RREQ_ORIG_SEQ_BYTE])
+        return 1;
+
+    return 0;
+
+}
+
+void net_handle_rrep ( uint8_t *packet){
+
+    net_rt_entry *rt_entry_dest = route_table(packet[RREP_RREQ_DEST_ADDRESS]);
+
+    if((rt_entry_dest->dest_seq < packet[RREP_DEST_SEQ_BYTE]) || ((rt_entry_src->dest_seq == packet[RREP_DEST_SEQ_BYTE]) && rt_entry_src->hop_count > packet[RREP_HOP_COUNT_BYTE] ) ){
     
-    net_rt_entry *rt_entry = route_table(packet[SRC_ADDRESS_BYTE]);
-    rt_entry->dest_node = packet[RREQ_ORIG_ADDRESS_BYTE];
-    rt_entry->next_hop = packet[SRC_ADDRESS_BYTE];
-    rt_entry->dest_seq = packet[RREQ_ORIG_SEQ_BYTE];
-    rt_entry->hop count = packet[RREQ_HOP_COUNT_BYTE] + 1;
-
-
-
-
 
 }
