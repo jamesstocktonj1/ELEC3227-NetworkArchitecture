926cba415f9d194b78aa2a819c3bb3de4c9ff291
Samuel Simpson <s.simpson41.ss@googlemail.com>
2022-12-02 13:42:37 +0000

    Initial code for interfacing with RFM12B library

--- include/datalink_phy_interface.h
+++ include/datalink_phy_interface.h
@@ -4,6 +4,21 @@
 #include "datalink.h"
 
 /**
+ * This function queues data to transmit on the RFM12B module
+ * @param length The length of the data
+ * @param data A pointer to the data
+ * @returns 0 - Success, otherwise error code
+*/
+int8_t dll_rf_tx(uint8_t length, uint8_t *data);
+
+/**
+ * This function checks and receives data when available from the RFM12B module
+ * @param data A pointer to the receive buffer to write to
+ * @returns 0 - No data, otherwise the length of the received data
+*/
+uint8_t dll_rf_rx(uint8_t *data);
+
+/**
  * This function is responsible with transmitting the DLL frames with 0.05p-CSMA on the RFM12B module
 */
 void dll_rf_tick();
--- src/datalink_phy_interface.c
+++ src/datalink_phy_interface.c
@@ -1,5 +1,51 @@
 #include "../include/datalink_phy_interface.h"
 
+#include "string.h"
+
+// Include the library for the RFM12 module and the UART
+#include "../include/rfm12_config.h"
+#include "../lib/librfm12/src/rfm12.h"
+#include "../lib/librfm12/src/include/rfm12_hw.h" // For RFM12_STATUS_RSSI
+#include "../lib/librfm12/src/include/rfm12_spi.c" // For rfm12_read
+
+#define MIN(x, y) (((x) < (y)) ? (x) : (y))
+
+uint16_t is_carrier_present();
+
+int8_t dll_rf_tx(uint8_t length, uint8_t *data) {
+    if (ctrl.txstate != STATUS_FREE) return -1; // TX buffer not free
+
+    if (rfm12_tx(length, 0, data) != RFM12_TX_ENQUEUED) {
+        return -2; // Failed to enqueue data
+    }
+
+    return 0;
+}
+
+uint8_t dll_rf_rx(uint8_t *data) {
+    if (rfm12_rx_status() != STATUS_COMPLETE) return 0;
+    uint8_t length = rfm12_rx_len();
+	memcpy(data, rfm12_rx_buffer(), length);
+	rfm12_rx_clear();
+    return length;
+}
+
 void dll_rf_tick() {
-    
+    //don't disturb RFM12 if transmitting or receiving
+	if (ctrl.rfm12_state != STATE_RX_IDLE) return;
+
+    //do we have something to transmit?
+    if (ctrl.txstate != STATUS_OCCUPIED) return;
+
+	if (is_carrier_present()) return;
+
+	rfm12_start_tx();
+}
+
+uint16_t is_carrier_present() {
+	RFM12_INT_OFF();
+	uint16_t status = rfm12_read(RFM12_CMD_STATUS);
+	RFM12_INT_ON();
+
+	return status & RFM12_STATUS_RSSI;
 }
