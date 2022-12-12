b2951dcf435fc5cf7e037e98cb16ac2dc3e6e346
Samuel Simpson <s.simpson41.ss@googlemail.com>
2022-12-12 11:07:29 +0000

    Add DLL code to main

--- main.c
+++ main.c
@@ -1,7 +1,6 @@
 #include <avr/io.h>
+#include <avr/interrupt.h>
 #include <util/delay.h>
-
-#include "include/rfm12.h"
 
 #include "include/io.h"
 #include "include/serial.h"
@@ -11,6 +10,7 @@
 #include "include/application.h"
 #include "include/datalink.h"
 #include "include/network.h"
+#include "include/datalink_phy_interface.h"
 
 
 #define NODE1
@@ -24,25 +24,31 @@
 #define APP_ADDR 0x01
 #endif
 
+#define MAC_ADDR APP_ADDR
+
 
 void poll_network_stack(void);
 
+uint8_t rx_packet[DLL_MAX_PACKET_SIZE] = {0};
+uint8_t tx_frame[DLL_MAX_FRAME_SIZE] = {0};
+uint8_t rx_frame[DLL_MAX_FRAME_SIZE] = {0};
 
 int main() {
     init_pins();
     init_serial();
     init_timer();
 
-    rfm12_init();
+    // TODO: Seed PRNG
 
+    printf("Initialising...");
     application_init();
     transport_init();
     // network_init();
     dll_init();
+    sei(); // enable interrupts
+    printf("Done\n");
 
-    sei();
-
-    uint8_t t = 500;
+    uint16_t t = 500;
 
     uint8_t i;
     uint8_t rxLength;
@@ -57,13 +63,14 @@ int main() {
         }
         else {
             uint8_t data[5] = {0x00, 0x01, 0x02, 0x03, 0x04};
-            uint8_t sent = application_handle_tx(data, 5, LIGHT_PORT, DEST_ADDR);
+            uint8_t data_length = 5;
+            uint8_t sent = application_handle_tx(data, data_length, LIGHT_PORT, DEST_ADDR);
 
-            printf("Data TX: ");
+            printf("Data TX:");
 
             if(sent) {
-                for(i=0; i<rxLength; i++) {
-                    printf("0x%04x, ");
+                for(i=0; i<data_length; i++) {
+                    printf(" %02x", data[i]);
                 }
                 printf("\n");
             }
@@ -77,11 +84,11 @@ int main() {
         
         rxLength = application_handle_rx(rxBuffer, NULL);
         if(rxLength) {
-            printf("Data RX: ");
+            printf("Data RX:");
             
             uint8_t i;
             for(i=0; i<rxLength; i++) {
-                printf("0x%04x, ");
+                printf(" %02x", rxBuffer[i]);
             }
             printf("\n");
         }
@@ -106,4 +113,32 @@ void poll_network_stack() {
 
 
     // handle data link layer
+    int8_t error = 0;
+
+    while (dll_has_rx_packet()) {
+        uint8_t length = dll_get_rx_packet(rx_packet);
+        length++;
+        // SEND TO NET LAYER
+    }
+
+    // if (net_has_tx_packet) {
+    //     if (!dll_has_tx_frame()) {
+    //         uint8_t queued = dll_queue_tx_net_packet((uint8_t*)msg, sizeof(msg), DLL_BROADCAST_ADDR);
+    //         if (!queued) printf("Failed to queue TX packet\n");
+    //     }
+    // }
+
+    if (dll_rf_can_tx() && dll_has_tx_frame()) {
+        uint8_t length = dll_get_tx_frame(tx_frame, MAC_ADDR);
+        error = dll_rf_tx(tx_frame, length);
+        if (error) printf("RF TX Error: %i\n", error);
+    }
+
+    uint8_t length = dll_rf_rx(rx_frame);
+    if (length) {
+        error = dll_receive_dll_frame(rx_frame, length, MAC_ADDR);
+        if (error) printf("DLL RX Error: %i\n", error);
+    }
+
+    dll_rf_tick();
 }
