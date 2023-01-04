483f13712cb7fac338a6b2bb1b41ddefca177116
Quphoria <s.simpson41.ss@googlemail.com>
2023-01-04 16:44:14 +0000

    DLL Test code

--- test/datalink_test.c
+++ test/datalink_test.c
@@ -1,7 +1,51 @@
 #include "datalink_test.h"
 
+#include <string.h>
+
+#include "../include/datalink.h"
+#include "../include/util.h"
+
+uint8_t get_next_prng_value();
+void serialisation_test();
 
 void main_datalink_test() {
 
-    fprintf(stderr, "main_datalink_test\n");
+    printf("main_datalink_test\n");
+    dll_init();
+
+    
+}
+
+void serialisation_test() {
+
+}
+
+
+
+uint8_t get_next_prng_value() {
+    uint8_t seed = prng();
+    prng_seed(seed);
+    uint8_t x = prng();
+    prng_seed(seed);
+    return x;
+}
+
+void gen_frames() {
+    char msg[] = "Goodbye World!!!";
+    uint8_t len = sizeof(msg);
+    uint8_t dest = 0x05;
+    uint8_t src = 0x42;
+
+    if (dll_queue_tx_net_packet((uint8_t*)msg, len, dest)) PASS
+    else FAIL    
+
+    uint8_t frame[DLL_MAX_FRAME_SIZE] = {0};
+    uint8_t length = 0;
+
+    while (dll_has_tx_frame()) {
+        length = dll_get_tx_frame(frame, src);
+        printf("Frame %d :", length);
+        for (uint8_t i = 0; i < length; i++) printf(", 0x%02x", frame[i]);
+        printf("\n");
+    }
 }
